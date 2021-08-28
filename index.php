7b0b8adba1818d977905f798e1083809670653d1
Souhail Merroun <souhail.merroun1996@gmail.com>
2021-08-28 23:26:23 +0200

    https://www.php.net/manual/en/tutorial.forms.php

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    
-    <?php echo '<p>Hello World</p>'; ?>
 
+<body>
+    <form action="action.php" method="post">
+        <p>Your name: <input type="text" name="name" /></p>
+        <p>Your age: <input type="text" name="age" /></p>
+        <p><input type="submit" /></p>
+    </form>
 </body>
+
 </html>
